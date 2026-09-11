4bcb0e45b397f7ec6f2b742493822c9f10473550
kirin <241276044@smail.nju.edu.cn>
2026-09-12 00:35:03 +0800

    Delete achievement directory

--- achievement/e4/undefined.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-  int a[10] = {0};
-  printf("a[10] = %d\n", a[10]);
-  return 0;
-}
--- achievement/e4/unspecified.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void f(int x, int y) {
-  printf("x = %d, y = %d\n", x, y);
-}
-int main() {
-  int i = 1;
-  f(i ++, i ++);
-  return 0;
-}
